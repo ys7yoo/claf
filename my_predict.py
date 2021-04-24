6f1fa55e3be7e2b9050cd8b7fcf5a795c47d71a0
yyoo@mini <yyoo@mini>
2021-04-25 00:57:49 +0900

    simplified further

--- my_predict.py
+++ my_predict.py
@@ -1,42 +1,35 @@
 # -*- coding: utf-8 -*-
 from claf.config import args
-from claf.config.utils import set_global_seed
+# from claf.config.utils import set_global_seed
 from claf.learn.experiment import Experiment
 from claf.learn.mode import Mode
 
 import torch
 
+if __name__ == "__main__":
+    experiment = Experiment(Mode.PREDICT, args.config(mode=Mode.PREDICT))
 
-def predict(model, raw_feature, raw_to_tensor_fn, arguments):
+    # set_global_seed(experiment.config.seed_num)  # For Reproducible
+
+    assert experiment.mode.endswith(Mode.PREDICT)
+    raw_feature, raw_to_tensor_fn, arguments = experiment.set_predict_mode()
+
+    assert raw_feature is not None
+    assert raw_to_tensor_fn is not None
+    # result = experiment.trainer.predict(
+    #     raw_feature,
+    #     raw_to_tensor_fn,
+    #     arguments,
+    #     interactive=arguments.get("interactive", False),
+    # )
+
+    model = experiment.trainer.model
+
     model.eval()
     with torch.no_grad():
         tensor_feature, helper = raw_to_tensor_fn(raw_feature)
         output_dict = model(tensor_feature)
 
-        return model.predict(output_dict, arguments, helper)
+        result = model.predict(output_dict, arguments, helper)
 
-
-if __name__ == "__main__":
-    experiment = Experiment(Mode.PREDICT, args.config(mode=Mode.PREDICT))
-
-    set_global_seed(experiment.config.seed_num)  # For Reproducible
-
-    assert experiment.mode.endswith(Mode.PREDICT)
-    raw_features, raw_to_tensor_fn, arguments = experiment.set_predict_mode()
-
-    assert raw_features is not None
-    assert raw_to_tensor_fn is not None
-    # result = experiment.trainer.predict(
-    #     raw_features,
-    #     raw_to_tensor_fn,
-    #     arguments,
-    #     interactive=arguments.get("interactive", False),
-    # )
-    model = experiment.trainer.model
-    result = predict(model,
-                     raw_features,
-                     raw_to_tensor_fn,
-                     arguments,
-    )
-
-    print(f"Predict: {result}")
+        print(f"Predict: {result}")
